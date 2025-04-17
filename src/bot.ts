169d515b233fce8fde92d26d7c87a6247d412d59
Ahmed Mahmoud <alrba2003@gmail.com>
2025-04-17 11:01:17 +0200

    feat: implement the ``blackList``, and ``belovedList`` functionality

--- src/bot.ts
+++ src/bot.ts
@@ -2,6 +2,18 @@ import { Client, GatewayIntentBits, VoiceState } from "discord.js";
 import { API_URL, DISCORD_TOKEN } from "./constants/env";
 import axios from "axios";
 
+enum User {
+  S7S = "1001159676777484308",
+  SHE3BO = "1264961784952000563",
+  ALAA = "1352026718487056545",
+  TAREK = "1276608261872816189",
+  KERO = "538763470091583498",
+  MAHMOUD = "1352115973108535385",
+}
+
+const BLACK_LIST: User[] = [];
+const BELOVED_LIST: User[] = [];
+
 type Event = "joined" | "left";
 interface ILogEntry {
   user: {
@@ -35,10 +47,21 @@ export class SomionBot {
       async (oldState: VoiceState, newState: VoiceState) => {
         const isJoined = !oldState.channelId && newState.channelId;
         const isLeft = oldState.channelId && !newState.channelId;
+        const User = newState.member?.id;
+        const isUserBlocked = this.blackListUsers(BLACK_LIST, User!);
 
-        if (isJoined || isLeft) {
+        if ((isJoined || isLeft) && !isUserBlocked) {
           const logEntry = this.extractLogEntry(oldState, newState);
+          // Update the timestamp if the user is a beloved user and left the voice channel to avoid double coun
+          if (isLeft) {
+            logEntry.timestamp = this.belovedList(
+              BELOVED_LIST,
+              User!,
+              new Date(logEntry.timestamp)
+            ).toISOString();
+          }
 
+          console.log(logEntry);
           const res = await axios.post(API_URL, logEntry);
           const data = res.data;
 
@@ -78,6 +101,26 @@ export class SomionBot {
     };
 
     return logEntry;
+  }
+
+  private blackListUsers(blockedUsers: User[], userId: string): boolean {
+    if (blockedUsers.includes(userId as User)) {
+      console.log(`🚫 '${userId}' has been blocked`);
+      return true;
+    }
+    return false;
+  }
+
+  private belovedList(
+    belovedUsers: User[],
+    userId: string,
+    leftDate: Date
+  ): Date {
+    if (belovedUsers.includes(userId as User)) {
+      console.log(`❤️ '${userId}' is a beloved user`);
+      return new Date(leftDate.getTime() + 2 * 60 * 60 * 1000);
+    }
+    return leftDate;
   }
 
   public start() {
